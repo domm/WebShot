ec15e94d902387f15d99103c153e7a200e027d4e
Thomas Klausner <domm@plix.at>
2012-08-15 15:03:12 +0200

    perltidy (sort of..)

--- lib/WebShot/Script/GenerateThumbnails.pm
+++ lib/WebShot/Script/GenerateThumbnails.pm
@@ -5,12 +5,20 @@ use MooseX::Types::Path::Class;
 use WebShot::Schema;
 use WebShot::Thumbnail;
 
-has 'root' => (is=>'ro',isa=>'Path::Class::Dir',coerce=>1,required=>1);
-has 'all' => (is=>'ro',isa=>'Bool',default=>0);
-has 'schema' => (is=>'ro', isa=>'DBIx::Class::Schema',required=>1,lazy_build=>1);
+has 'root' =>
+  ( is => 'ro', isa => 'Path::Class::Dir', coerce => 1, required => 1 );
+has 'all' => ( is => 'ro', isa => 'Bool', default => 0 );
+has 'schema' => (
+    is         => 'ro',
+    isa        => 'DBIx::Class::Schema',
+    required   => 1,
+    lazy_build => 1
+);
+
 sub _build_schema {
     my $self = shift;
-    return WebShot::Schema->connect("dbi:SQLite:dbname=".$self->root->file('webshot.db'));
+    return WebShot::Schema->connect(
+        "dbi:SQLite:dbname=" . $self->root->file('webshot.db') );
 }
 
 sub run {
@@ -18,14 +26,13 @@ sub run {
 
     my $list = $self->schema->resultset('Website');
     if ( !$self->all ) {
-        $list = $list->search({processed=>0});
+        $list = $list->search( { processed => 0 } );
     }
 
     my $runner = WebShot::Thumbnail->new(
-        root=>$self->root->subdir('static'),
-        list=>$list,
+        root => $self->root->subdir('static')
     );
-    $runner->thumbnail;
+    $runner->thumbnail_resultset($list);
 }
 
 __PACKAGE__->meta->make_immutable;
--- lib/WebShot/Thumbnail.pm
+++ lib/WebShot/Thumbnail.pm
@@ -2,23 +2,28 @@ package WebShot::Thumbnail;
 use Moose;
 use Imager;
 
-has 'size' => (is=>'ro',isa=>'Int',default=>'32');
-has 'list' => (is=>'ro',isa=>'DBIx::Class::ResultSet',required=>1);
-has 'root' => (is=>'ro',isa=>'Path::Class::Dir',required=>1);
+has 'size' => ( is => 'ro', isa => 'Int', default => '32' );
+has 'root' => ( is => 'ro', isa => 'Path::Class::Dir', required => 1 );
 
-sub thumbnail {
-    my $self = shift;
+sub thumbnail_resultset {
+    my ( $self, $rs ) = @_;
 
-    my $list = $self->list;
-    while (my $item = $list->next) {
-        my $image = Imager->new;
-        my $src = $self->root->file('shots',$item->image);
-        $image->read(file=>$src->stringify) || die "Cannot read: $src".$image->errstr;
-        my $scaled = $image->scale(xpixels => $self->size) || die "Cannot scale $src: ".$image->errstr;
-        $scaled->write(file=>$self->root->file('thumbnails',$item->image)->stringify);
-
-        $item->update({processed=>1});
+    while ( my $item = $rs->next ) {
+        $self->thumbnail_item($item);
     }
+}
+
+sub thumbnail_item {
+    my ( $self, $item ) = @_;
+    my $image  = Imager->new;
+    my $src    = $self->root->file( 'shots', $item->image );
+    my $target = $self->root->file( 'thumbnails', $item->image );
+
+    $image->read( file => $src->stringify ) || die "Cannot read: $src" . $image->errstr;
+    my $scaled = $image->scale( xpixels => $self->size ) || die "Cannot scale $src: " . $image->errstr;
+    $scaled->write( file => $target->stringify) || die "Cannot write to $target: " . $image->errstr;
+
+    $item->update( { processed => 1 } );
 }
 
 __PACKAGE__->meta->make_immutable;
