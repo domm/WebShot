cb4452238b7b4dd66cdce76a6b59200885c4fe42
Thomas Klausner <domm@plix.at>
2012-08-19 23:12:16 +0200

    is_interactive

--- lib/WebShot/InitLogger.pm
+++ lib/WebShot/InitLogger.pm
@@ -3,9 +3,20 @@ use strict;
 use warnings;
 use 5.010;
 
+use IO::Interactive qw(is_interactive);
 use Log::Any::Adapter;
 use Log::Log4perl;
-Log::Log4perl::init('/home/domm/perl/talks/logging/WebShot/log4perl.conf');
-Log::Any::Adapter->set('Log::Log4perl');
 
+if ( is_interactive() ) {
+    Log::Log4perl::init(
+      '/home/domm/perl/talks/logging/WebShot/log4perl.conf'
+    );
+}
+else {
+    Log::Log4perl::init(
+      '/home/domm/perl/talks/logging/WebShot/log4perl_noninteractive.conf'
+    );
+}
+
+Log::Any::Adapter->set('Log::Log4perl');
 1;
