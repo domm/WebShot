b81c263a868bec605dbbf462f113d9bd4a01e2cf
Thomas Klausner <domm@plix.at>
2012-08-20 00:41:22 +0200

    use syslog

--- lib/WebShot/InitLogger.pm
+++ lib/WebShot/InitLogger.pm
@@ -5,39 +5,20 @@ use 5.010;
 
 use IO::Interactive qw(is_interactive);
 use Log::Any::Adapter;
-use Log::Dispatch;
 
 if ( $ENV{HARNESS_ACTIVE} ) {
     Log::Any::Adapter->set( 'Test' );
 }
 else {
-    my $log = Log::Dispatch->new(
-        outputs => [
-            [ 'File',
-            filename  => '/var/log/webshot/log_dispatch.log',
-            min_level => 'debug',
-            mode      => 'append',
-            ],
-        ],
-        callbacks => [
-            sub {
-                my %msg = @_;
-                return sprintf(
-                    "[%s] %s(%s): %s\n",
-                    scalar localtime(time),
-                    $0, $$,
-                    $msg{message}
-                )
-            }
-    ]);
-    if ( is_interactive() ) {
-        use Log::Dispatch::Screen;
-        $log->add( Log::Dispatch::Screen->new(
-            name      => 'screen',
-            min_level => 'debug',
-        ));
-    }
-    Log::Any::Adapter->set( 'Dispatch', dispatcher => $log );
+    use Unix::Syslog qw(:macros);
+    my $opts = is_interactive()
+               ? LOG_PID|LOG_PERROR
+               : LOG_PID;
+    Log::Any::Adapter->set(
+        'Syslog',
+        options  => $opts,
+        facility => LOG_LOCAL1
+    );
 }
 
 1;
