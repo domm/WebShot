2fb1f0788478ca25bc96155a3051518270b5b1ba
Thomas Klausner <domm@plix.at>
2012-08-19 21:44:44 +0200

    use Log::Any in GenerateThumbnails

--- lib/WebShot/Script/GenerateThumbnails.pm
+++ lib/WebShot/Script/GenerateThumbnails.pm
@@ -3,17 +3,8 @@ use Moose;
 use MooseX::Types::Path::Class;
 use WebShot::Schema;
 use WebShot::Thumbnail;
-use Log::Any::Adapter;
-use Log::Dispatch;
-my $log = Log::Dispatch->new( outputs => [
-    [ 'File',
-      filename  => '/var/log/webshot.log',
-      min_level => 'debug',
-      newline   => 1,
-      mode      => 'append',
-    ],
-]);
-Log::Any::Adapter->set( 'Dispatch', dispatcher => $log );
+use WebShot::InitLogger;
+use Log::Any qw($log);
 
 with qw(MooseX::Getopt);
 
